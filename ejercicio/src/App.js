b57c773e833f54e41456288844ea9f127fb8778e
richardmarrugo17 <richard17_m@hotmail.com>
2022-02-04 17:30:58 -0500

    Segundo Commit

--- ejercicio/src/App.js
+++ ejercicio/src/App.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, {useState, useRef} from "react";
 import { TodoList } from "./components/TodoList";
+import { v4 as uuidv4} from "uuid";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export function App(){
-    const listas = [{id:"1", task:"Tarea 1", completed:false}];
+    const [listas, setListas] = useState([{id:"1", task:"Tarea", completed:false},]);
+    const taskRef = useRef();
+    const AgregarTareas = () =>{
+        const task = taskRef.current.value;
+        if (task === "") return;
+        setListas ((prevTasks) =>{
+            return [...prevTasks, {id: uuidv4(), task, completed:false}];
+        });
+        taskRef.current.value = null // Limpiar el input
 
+    };
     return (
-        <div>
-            <TodoList listas= {listas}/>
-            <input type="text" placeholder="Nueva Tarea"/>
-            <button>Agregar</button>
-            <button>Eliminar</button>
+        <div className="container py-5">
+            <div className="card">
+                <div className="card-header">
+                    <h3>Lista de Tareas</h3>
+                </div>
+                <div className="card-body">
+                    <TodoList listas= {listas}/>
+                    <input ref={taskRef} type="text" placeholder="Nueva Tarea"/>
+                    <button onClick={AgregarTareas}>Agregar</button>
+                    <button>Eliminar</button>
+                </div>
+            </div>
         </div>
     );
 }
